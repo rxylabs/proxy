f80ba969f70a353dc24408afafa6db13447e91ad
Andrew Hampton <drew@firma8.com>
2016-01-13 19:53:51 +0000

    Adding basic proxy

--- main.go
+++ main.go
@@ -2,31 +2,42 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
-
-	"github.com/valyala/fasthttp"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
 )
 
 var config struct {
 	googleToken string
 	target      string
+	port        string
 }
 
 func init() {
 	flag.StringVar(&config.googleToken, "googleToken", "", "Google token")
 	flag.StringVar(&config.target, "target", "http://www.apple.com", "Site to proxy")
+	flag.StringVar(&config.port, "port", "5000", "Port to use")
 	flag.Parse()
 
-	if config.googleToken == nil {
+	if config.googleToken == "" {
 		log.Panic("googleToken is required")
 	}
 
-	if config.target == nil {
+	if config.target == "" {
 		log.Panic("target is required")
 	}
 
 }
 
 func main() {
+	targetURL, err := url.Parse(config.target)
+	if err != nil {
+		log.Panic("Error parsing target: ", config.target, ": ", err)
+	}
 
+	rp := httputil.NewSingleHostReverseProxy(targetURL)
+
+	http.ListenAndServe(fmt.Sprintf(":%s", config.port), rp)
 }
